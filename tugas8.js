324bb536f0a5c660c1d113ea4e5d067b61d5b671
mazajiputra <mazajiputra@gmail.com>
2021-10-29 23:21:26 +0700

    Kumpul Tugas

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function tugas() {
+    var teman = {
+        Nama        : 'Tono',
+        tahunLahir  : 2000,
+    }
+
+    console.log(teman)
+    teman.tahunLahir=2002;
+    console.log(teman)
+}
+tugas()
